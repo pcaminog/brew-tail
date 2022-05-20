f6552f0843bb9c0cbd7dd156ffeb47d1ea59643d
Pablo Camino <pcaminog@gmail.com>
2022-05-20 16:13:32 -0500

    00.2

--- components/Auth.js
+++ components/Auth.js
@@ -20,11 +20,11 @@ export default function Auth() {
   };
 
   return (
-      <div>
-        <h1>
+      <div className="flex items-center justify-center h-screen bg-pri font-mono">
+        <h1 className=" font-extrabold text-8xl text-cuar">
           Brew It ☕️ Login
         </h1>
-        <p >
+        <p className="font-extrabold text-2xl text-s text-cuar">
           Sign in via magic link with your email below
         </p>
         <div >
@@ -39,6 +39,8 @@ export default function Auth() {
           />
         </div>
           <button 
+          className="flex font-extrabold justify-center items-center border-2 border-ter rounded-lg p-2 w-full bg-sec hover:border-4 hover:border-ter hover:bg-sec hover:text-cuar shadow-ter shadow-md text-xl"
+
             onClick={(e) => {
               e.preventDefault();
               handleLogin(email);
