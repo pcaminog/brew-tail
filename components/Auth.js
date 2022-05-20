64bf2f1c82f4b65b64f427e1f58f9795c63868dd
Pablo Camino <pcaminog@gmail.com>
2022-05-20 16:15:36 -0500

    00.2.

--- components/Auth.js
+++ components/Auth.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { supabase } from "../utils/supabase";
 
-
 export default function Auth() {
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
@@ -20,16 +19,16 @@ export default function Auth() {
   };
 
   return (
-      <div className="flex items-center justify-center h-screen bg-pri font-mono">
+    <div className="flex items-center justify-center h-screen bg-pri font-mono">
+      <div className="grid place-items-center w-3/4 gap-10 m-5">
         <h1 className=" font-extrabold text-8xl text-cuar">
           Brew It ☕️ Login
         </h1>
         <p className="font-extrabold text-2xl text-s text-cuar">
           Sign in via magic link with your email below
         </p>
-        <div >
-
-        <input
+        <div>
+          <input
             maxLength={50}
             minLength={5}
             type="email"
@@ -38,17 +37,17 @@ export default function Auth() {
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
-          <button 
+        <button
           className="flex font-extrabold justify-center items-center border-2 border-ter rounded-lg p-2 w-full bg-sec hover:border-4 hover:border-ter hover:bg-sec hover:text-cuar shadow-ter shadow-md text-xl"
-
-            onClick={(e) => {
-              e.preventDefault();
-              handleLogin(email);
-            }}
-            disabled={loading}
-          >
-            <span>{loading ? "Loading" : "Send magic link"}</span>
-          </button>
-        </div>
+          onClick={(e) => {
+            e.preventDefault();
+            handleLogin(email);
+          }}
+          disabled={loading}
+        >
+          <span>{loading ? "Loading" : "Send magic link"}</span>
+        </button>
+      </div>
+    </div>
   );
 }
