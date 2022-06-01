e1278e836b7b83fb4a6d40f3d8b6f084e6ea2e04
Pablo Camino <pcaminog@gmail.com>
2022-06-01 10:15:36 -0500

    00.10

--- components/Inputnext.js
+++ components/Inputnext.js
@@ -1,18 +1,17 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import { supabase } from "../utils/supabase";
 import { useRouter } from "next/router";
 
 function inputnext() {
-
-  const [session, setSession] = useState(null)
+  const [session, setSession] = useState(null);
 
   useEffect(() => {
-    setSession(supabase.auth.session())
+    setSession(supabase.auth.session());
 
     supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
+      setSession(session);
+    });
+  }, []);
 
   const [brew, setBrew] = useState({
     machine: "",
@@ -26,10 +25,6 @@ function inputnext() {
   const user = supabase.auth.user();
 
   const router = useRouter();
-
-  const refreshData = () => {
-    router.replace(router.asPath);
-  };
 
   function handleChange(event) {
     event.preventDefault();
@@ -53,115 +48,92 @@ function inputnext() {
       time: "",
       model: "",
     });
-    refreshData();
+    router.replace(router.asPath);
   };
-
-  if (!session) {
-    
-    return (
-        <button type="button" 
-        className="flex font-extrabold justify-center items-center border-2 border-ter rounded-lg p-2 w-full bg-sec hover:border-4 hover:border-ter hover:bg-sec hover:text-cuar shadow-ter shadow-md text-xl"
-        onClick={() => router.push('/auth')}>
-          Sign Up!
-        </button>
-    )
-  }
 
   return (
     <div>
-      <div className=" items-center justify-center grid grid-cols-6  grid-rows-2 gap-5 p-6 gap-y-0">
-        <p
-        className="font-extrabold text-2xl text-cuar"
-        >MACHINE 🤖</p>
-        <p
-        className="font-extrabold text-2xl text-cuar"
-        >MODEL ⚙️</p>
-        <p
-        className="font-extrabold text-2xl text-cuar"
-        >WATER 💧</p>
-        <p
-        className="font-extrabold text-2xl text-cuar"
-        >COFFEE 🫘</p>
-        <p
-        className="font-extrabold text-2xl text-cuar"
-        >GRAMS ⚖️</p>
-        <p
-        className="font-extrabold text-2xl text-cuar"
-        >6TIME ⏱</p>
+      {!session ? (
+        <button
+          onClick={() => router.push("/auth")}
+          className="flex font-extrabold justify-center items-center border-2 border-ter rounded-lg p-2 w-full bg-sec hover:border-4 hover:border-ter hover:bg-sec hover:text-cuar shadow-ter shadow-md text-xl"
+        >
+          {" "}
+          Sign Up
+        </button>
+      ) : (
+        <div>
+          <div className=" items-center justify-center grid grid-cols-6  grid-rows-2 gap-5 p-6 gap-y-0">
+            <p className="font-extrabold text-2xl text-cuar">MACHINE 🤖</p>
+            <p className="font-extrabold text-2xl text-cuar">MODEL ⚙️</p>
+            <p className="font-extrabold text-2xl text-cuar">WATER 💧</p>
+            <p className="font-extrabold text-2xl text-cuar">COFFEE 🫘</p>
+            <p className="font-extrabold text-2xl text-cuar">GRAMS ⚖️</p>
+            <p className="font-extrabold text-2xl text-cuar">TIME ⏱</p>
 
-        <input
-          className="rounded-lg p-2 w-full border-2 border-ter bg-sec hover:border-4 hover:border-ter hover:bg-sec hover:text-cuar shadow-ter shadow-md text-xl"
-          id="machine"
-          maxLength={50}
-          minLength={5}
-          label="Machine"
-          placeholder="Brevilles, Nespresso,.."
-          value={brew.machine}
-          color="warning"
-          onChange={handleChange}
-        />
-        <input
-          className=" border-2 border-ter rounded-lg p-2 w-full bg-sec hover:border-4 hover:border-ter hover:bg-sec hover:text-cuar shadow-ter shadow-md text-xl"
-          id="model"
-          maxLength={50}
-          minLength={5}
-          label="Model"
-          color="warning"
-          placeholder="Barista, Tassimo,.."
-          value={brew.model}
-          onChange={handleChange}
-        />
-        <input
-          className=" border-2 border-ter rounded-lg p-2 w-full bg-sec hover:border-4 hover:border-ter hover:bg-sec hover:text-cuar shadow-ter shadow-md text-xl"
-          id="water_type"
-          maxLength={20}
-          minLength={3}
-          label="Water 💦"
-          placeholder="Mineral, Fitered,.."
-          color="warning"
-          value={brew.water_type}
-          onChange={handleChange}
-        />
-        <input
-          className=" border-2 border-ter rounded-lg p-2 w-full bg-sec hover:border-4 hover:border-ter hover:bg-sec hover:text-cuar shadow-ter shadow-md text-xl"
-          id="coffee"
-          maxLength={50}
-          minLength={5}
-          label="Coffee 🫘"
-          placeholder="Lavazza supreme, Seattle Coffee,.."
-          color="warning"
-          value={brew.coffee}
-          onChange={handleChange}
-        />
-        <input
-          className=" border-2 border-ter rounded-lg p-2 w-full bg-sec hover:border-4 hover:border-ter hover:bg-sec hover:text-cuar shadow-ter shadow-md text-xl"
-          id="grams"
-          type="number"
-          max={999}
-          label="Weight ⚖️ (Grams)"
-          placeholder="In grams"
-          color="warning"
-          value={brew.grams}
-          onChange={handleChange}
-        />
-        <input
-          className=" border-2 border-ter rounded-lg p-2 w-full bg-sec hover:border-4 hover:border-ter hover:bg-sec hover:text-cuar shadow-ter shadow-md text-xl"
-          id="time"
-          type="number"
-          max={999}
-          label="Time ⏱ (Seconds)"
-          placeholder="In seconds"
-          color="warning"
-          value={brew.time}
-          onChange={handleChange}
-        />
-      </div>
-      <button
-      className="flex font-extrabold justify-center items-center border-2 border-ter rounded-lg p-2 w-full bg-sec hover:border-4 hover:border-ter hover:bg-sec hover:text-cuar shadow-ter shadow-md text-xl"
-        onClick={() => postBrew(event)}
-      >
-        Add it ➕
-      </button>
+            <input
+              className="rounded-lg p-2 w-full border-2 border-ter bg-sec hover:border-4 hover:border-ter hover:bg-sec hover:text-cuar shadow-ter shadow-md text-xl"
+              id="machine"
+              maxLength={50}
+              minLength={5}
+              placeholder="Brevilles, Nespresso,.."
+              value={brew.machine}
+              onChange={handleChange}
+            />
+            <input
+              className=" border-2 border-ter rounded-lg p-2 w-full bg-sec hover:border-4 hover:border-ter hover:bg-sec hover:text-cuar shadow-ter shadow-md text-xl"
+              id="model"
+              maxLength={50}
+              minLength={5}
+              placeholder="Barista, Tassimo,.."
+              value={brew.model}
+              onChange={handleChange}
+            />
+            <input
+              className=" border-2 border-ter rounded-lg p-2 w-full bg-sec hover:border-4 hover:border-ter hover:bg-sec hover:text-cuar shadow-ter shadow-md text-xl"
+              id="water_type"
+              maxLength={20}
+              minLength={3}
+              placeholder="Mineral, Fitered,.."
+              value={brew.water_type}
+              onChange={handleChange}
+            />
+            <input
+              className=" border-2 border-ter rounded-lg p-2 w-full bg-sec hover:border-4 hover:border-ter hover:bg-sec hover:text-cuar shadow-ter shadow-md text-xl"
+              id="coffee"
+              maxLength={50}
+              minLength={5}
+              placeholder="Lavazza supreme, Seattle Coffee,.."
+              value={brew.coffee}
+              onChange={handleChange}
+            />
+            <input
+              className=" border-2 border-ter rounded-lg p-2 w-full bg-sec hover:border-4 hover:border-ter hover:bg-sec hover:text-cuar shadow-ter shadow-md text-xl"
+              id="grams"
+              type="number"
+              max={999}
+              placeholder="In grams"
+              value={brew.grams}
+              onChange={handleChange}
+            />
+            <input
+              className=" border-2 border-ter rounded-lg p-2 w-full bg-sec hover:border-4 hover:border-ter hover:bg-sec hover:text-cuar shadow-ter shadow-md text-xl"
+              id="time"
+              type="number"
+              max={999}
+              placeholder="In seconds"
+              value={brew.time}
+              onChange={handleChange}
+            />
+          </div>
+          <button
+            className="flex font-extrabold justify-center items-center border-2 border-ter rounded-lg p-2 w-full bg-sec hover:border-4 hover:border-ter hover:bg-sec hover:text-cuar shadow-ter shadow-md text-xl"
+            onClick={() => postBrew(event)}
+          >
+            Add it ➕
+          </button>
+        </div>
+      )}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Inputnext from "../components/Inputnext";
 import Tablenext from "../components/Tablenext";
 import { supabase } from "../utils/supabase";
+import  Head from 'next/head'
 
 function Home({ brews }) {
 
 
   return (
     <div className="flex items-center justify-center h-screen bg-pri font-mono">
+      <Head>
+    <title>{`Brew it ☕️`}</title>
+  </Head>
       <div className="grid place-items-center w-3/4 gap-10 m-5">
         <h1 className=" font-extrabold text-8xl text-cuar">Brew It ☕️</h1>
         <p className="font-extrabold text-2xl text-s text-cuar">
@@ -16,6 +20,7 @@ function Home({ brews }) {
         <h2 className="font-extrabold text-2xl text-cuar">
           Add your recipe 👇
         </h2>
+        {}
         <Inputnext />
       </div>
       <footer className="bg-pri text-cuar w-full bottom-0 flex absolute  items-center justify-center text-2xl border-t-4 border-t-ter m-4 p-4">
